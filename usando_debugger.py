54b78e08530a1a5bae7ee50e9873d976b4fa0b49
Carlos Henrique <olv.carlos@hotmail.com>
2024-09-11 12:00:21 -0300

    Versão 3 - testando debugger

---
+++ usando_debugger.py
@@ -0,0 +1,17 @@
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+
+    print(total)
+
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado')
+'''
+F9 MARCAR UM BREAKPOINT
+F5 ABRIR MODO DEBUG
+F10 Passar sobre uma linha, sem entrar em metodos internos
+F11 Passar sobre uma linha e ENTRAR em metodo interno
+'''
